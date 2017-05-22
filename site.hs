9b19e80b0dcde8a8163d80f2869b25e7b52674b6
Jason Shipman <jasonpshipman@gmail.com>
2017-05-21 22:07:13 -0400

    Add rule to copy Semantic UI CSS

--- site.hs
+++ site.hs
@@ -1,68 +1,56 @@
---------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
-import           Hakyll
 
+import Data.Monoid (mappend)
+import Hakyll
+import System.FilePath
 
---------------------------------------------------------------------------------
 main :: IO ()
-main = hakyll $ do
-    match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
-
+main =
+  hakyll $ do
+    match "semantic/dist/components/*.min.css" $ do
+      route $ customRoute $ ("css" </>) . takeFileName . toFilePath
+      compile copyFileCompiler
     match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
-
+      route idRoute
+      compile compressCssCompiler
+    match "images/*" $ do
+      route idRoute
+      compile copyFileCompiler
     match (fromList ["about.rst", "contact.markdown"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
-
+      route $ setExtension "html"
+      compile $
+        pandocCompiler >>=
+        loadAndApplyTemplate "templates/default.html" defaultContext >>=
+        relativizeUrls
     match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
-
+      route $ setExtension "html"
+      compile $
+        pandocCompiler >>= loadAndApplyTemplate "templates/post.html" postCtx >>=
+        loadAndApplyTemplate "templates/default.html" postCtx >>=
+        relativizeUrls
     create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
-
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
-
-
+      route idRoute
+      compile $ do
+        posts <- recentFirst =<< loadAll "posts/*"
+        let archiveCtx =
+              listField "posts" postCtx (return posts) `mappend`
+              constField "title" "Archives" `mappend`
+              defaultContext
+        makeItem "" >>= loadAndApplyTemplate "templates/archive.html" archiveCtx >>=
+          loadAndApplyTemplate "templates/default.html" archiveCtx >>=
+          relativizeUrls
     match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    defaultContext
-
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
-
+      route idRoute
+      compile $ do
+        posts <- recentFirst =<< loadAll "posts/*"
+        let indexCtx =
+              listField "posts" postCtx (return posts) `mappend`
+              constField "title" "Home" `mappend`
+              defaultContext
+        getResourceBody >>= applyAsTemplate indexCtx >>=
+          loadAndApplyTemplate "templates/default.html" indexCtx >>=
+          relativizeUrls
     match "templates/*" $ compile templateCompiler
 
-
---------------------------------------------------------------------------------
 postCtx :: Context String
-postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
-
+postCtx = dateField "date" "%B %e, %Y" `mappend` defaultContext
